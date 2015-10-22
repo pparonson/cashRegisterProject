f26e4ef323d412c63b63a0c2f970a58c1195e035
Cole Rathjen <colerathjen@gmail.com>
2015-10-22 14:47:09 -0600

    browser.js

--- clientSide/scripts/browser.js
+++ clientSide/scripts/browser.js
@@ -14,11 +14,15 @@ $(document).ready(function(){
 	//make a decision
 	var localStorageJson = localStorage.getItem("Account");
 	if (localStorageJson&&localStorageJson!=="undefined"){
+		$("#welcomePage").css("display","none");	//temporary
 		console.log(typeof localStorageJson)
 		console.log(localStorageJson)
 		var obj = JSON.parse(localStorageJson);
 		account=cashRegister.Account.loadJSON(obj);
 		presentAccount();
+		account.subscribe("change", function(){
+		presentAccount();	
+		});
 	}
 	if (!account){
 		var welcome=$("#welcomePage")
@@ -27,11 +31,30 @@ $(document).ready(function(){
 			account=cashRegister.Account.initializeAccount($("#SBalance").val(), $("#acctName").val());
 			welcome.css("display","none");
 			presentAccount();
+			account.subscribe("change", function(){
+			presentAccount();
+			})
 		})
 		//send to welcome page 
 		//initialize account by calling cashRegister.Account.initializeAccount(balance, acctname) and call presentAccount
 	}
 	
+	$("#submitDeposit").click(function(){
+		
+		account.addTransaction($("#transactionAmount").val()
+					,$("#transactionDate").val()
+					,$("#accountType").val()
+					,$("#memo").val());
+		
+	});
+	$("#submitWithdraw").click(function(){
+
+		account.addTransaction(($("#transactionAmount").val()*-1)
+					,$("#transactionDate").val()
+					,$("#accountType").val()
+					,$("#memo").val());
+		
+	});
 	
 	
 	
@@ -41,6 +64,7 @@ function presentAccount(){
 	var table=$("#mainTable");
 	table.empty();
 	table.append($("<tr g='from js'><th>Date</th><th>Amount</th><th>Type</th><th>Memo</th><th>Balance</th></tr>"));
+	
 	var row, i;
 	for (i=0;i<account.transactions.length;i++){
 		row = makeRow(account.transactions[i]);
@@ -55,7 +79,7 @@ function presentAccount(){
 		tr.append($("<td>"+transactionObj.memo+"</td>"));
 		return tr;
 	}
-	
+
 }
 
 $(window).unload(function(){
